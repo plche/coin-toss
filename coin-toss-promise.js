56ee3cca19d09595dec2c597f426aa2561d7a380
plche <che.percy.l@gmail.com>
2022-03-25 13:50:58 -0500

    Initial commit

---
+++ coin-toss-promise.js
@@ -0,0 +1,33 @@
+function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+}
+
+function getFiveHeads() {
+    let headsCount = 0;
+    let attempts = 0;
+    while(headsCount < 5) {
+        attempts++;
+        let result = tossCoin();
+        console.log(`${result} was flipped`);
+        if(result === "heads") {
+            headsCount++;
+        } else {
+            headsCount = 0;
+        }
+    }
+    return attempts;
+}
+
+const fiveHeadsPromise = new Promise((resolve, reject) => {
+    let attempts = getFiveHeads();
+    if (attempts > 0) {
+        resolve(`It took ${attempts} tries to flip five "heads"`);
+    } else {
+        reject(`Can't get to flip five "heads"`);
+    }
+});
+
+fiveHeadsPromise
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+console.log("This is run after the fiveHeadsPromise promise was call");
